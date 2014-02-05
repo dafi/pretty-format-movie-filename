9249e3f8536756a65965aded925a1383a8e6c836
Davide Ficano <davide.ficano@gmail.com>
2014-02-05 16:57:39 +0100

    Move files from torrent subdirectory to main directory

--- completed.sh
+++ completed.sh
@@ -3,6 +3,18 @@
 # Called by Transmission when a download complete
 
 if [ "$TR_TORRENT_DIR" != "" ] ; then
-	echo Renaming $TR_TORRENT_DIR/$TR_TORRENT_NAME >>/Users/dave/trash/000_torrent_complete.log 2>&1
-	/usr/local/bin/node `dirname $0`/renmovies.js "$TR_TORRENT_DIR/$TR_TORRENT_NAME" >>/Users/dave/trash/000_torrent_complete.log 2>&1
+	LOG_FILE="$TR_TORRENT_DIR/.torrent_fetch/torrents/torrent_complete.log"
+	mkdir -p `dirname $LOG_FILE`
+
+	echo Renaming $TR_TORRENT_DIR/$TR_TORRENT_NAME >>$LOG_FILE 2>&1
+	/usr/local/bin/node `dirname $0`/renmovies.js "$TR_TORRENT_DIR/$TR_TORRENT_NAME" >>$LOG_FILE 2>&1
+	if [ -d "$TR_TORRENT_DIR/$TR_TORRENT_NAME" ]
+	then
+		found=`find "$TR_TORRENT_DIR/$TR_TORRENT_NAME" -maxdepth 1 -type f -name "*.mp4"`
+		if [ -n "$found" ]
+		then
+			mv "$TR_TORRENT_DIR/$TR_TORRENT_NAME"/*.mp4 "$TR_TORRENT_DIR"
+			osascript -e "tell application \"Finder\" to delete POSIX file \"$TR_TORRENT_DIR/$TR_TORRENT_NAME\"" &>/dev/null
+		fi		
+	fi
 fi
