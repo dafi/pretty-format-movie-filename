81145276c6788a0034bc99b8df8b2448d815220a
Davide Ficano <davide.ficano@gmail.com>
2014-03-30 16:30:06 +0200

    Script to massive unzip and name formatting

---
+++ unzip_subs.sh
@@ -0,0 +1,12 @@
+ZIP_DIR=`cat subs.json | json_pp | grep outputPath | awk 'BEGIN {FS=":"} {print $2}'`
+
+for i in $ZIP_DIR/*.zip
+do
+	unzip $i -d $ZIP_DIR
+	rm $i
+done
+
+for i in $ZIP_DIR/*.srt
+do
+	node renmovies.js $i
+done
