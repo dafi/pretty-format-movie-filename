79b7836f6105778afba7ed2922710aac9393fb07
Davide Ficano <davide.ficano@gmail.com>
2014-07-06 18:30:39 +0200

    determine script directory and use it to build config path

--- unzip_subs.sh
+++ unzip_subs.sh
@@ -1,4 +1,5 @@
-ZIP_DIR=`cat subs.json | json_pp | grep outputPath | sed 's/"//g' | awk 'BEGIN {FS=":"} {print $2}'`
+DIR_NAME=`dirname $0`
+ZIP_DIR=`cat $DIR_NAME/subs.json | json_pp | grep outputPath | sed 's/"//g' | awk 'BEGIN {FS=":"} {print $2}'`
 
 for i in $ZIP_DIR/*.zip
 do
@@ -8,5 +9,5 @@ done
 
 for i in $ZIP_DIR/*.srt
 do
-	node renmovies.js $i
+	node $DIR_NAME/renmovies.js $i
 done
